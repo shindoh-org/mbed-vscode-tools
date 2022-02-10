4862e73655ce59188d6b637faeeb052d78c564b0
otenim <snake_istobelieve@keio.jp>
2022-02-11 07:51:14 +0900

    update

--- mbed_vscode_tools/mbed_vscode_tools.py
+++ mbed_vscode_tools/mbed_vscode_tools.py
@@ -1,7 +1,23 @@
+import click
+
+
+@click.group()
+def cmd():
+    pass
+
+
+@cmd.command()
+def doctor():
+    click.echo('doctor')
+
+
+@cmd.command()
+def update():
+    click.echo('update')
 
 
 def main():
-    print("mbed-vscode-tools")
+    cmd()
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 import setuptools
 
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
+
 setuptools.setup(
-    name="mbed-vscode-tools",
-    version="0.0.1",
-    install_requires=["mbed-tools"],
+    name='mbed-vscode-tools',
+    version='0.0.1',
+    install_requires=['click'],
     entry_points={
-        "console_scripts": [
-            "mbed-vscode-tools=mbed_vscode_tools.mbed_vscode_tools:main"]
+        'console_scripts': [
+            'mbed-vscode-tools=mbed_vscode_tools.mbed_vscode_tools:main']
     },
-    author="Keisuke Sugiura",
-    author_email="std.experimental.optional@gmail.com",
-    description="A command-line to facilitate vscode intellisense  for mbed-os.",
+    author='Keisuke Sugiura',
+    author_email='std.experimental.optional@gmail.com',
+    description='A command-line to facilitate vscode intellisense  for mbed-os.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sterngerlach/mbed-vscode-tools",
+    long_description_content_type='text/markdown',
+    url='https://github.com/sterngerlach/mbed-vscode-tools',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux"
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'
     ],
-    python_requires=">=3.7")
+    python_requires='>=3.7')
