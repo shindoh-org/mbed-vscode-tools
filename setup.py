42f9f3a72606d50047792f66367e20923f3e3a86
otenim <snake_istobelieve@keio.jp>
2022-02-11 06:21:11 +0900

    update

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-# coding: utf-8
-# setup.py
-
 import setuptools
+
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="mbed-config-gen",
+    name="mbed-vscode-tools",
     version="0.0.1",
     install_requires=["mbed-tools"],
     entry_points={
         "console_scripts": [
-            "mbed-config-gen=mbed_config_gen.mbed_config_gen:main"]
+            "mbed-vscode-tools=mbed_vscode_tools.mbed_vscode_tools:main"]
     },
     author="Keisuke Sugiura",
     author_email="std.experimental.optional@gmail.com",
-    description="Command-line tool to generate C/C++ headers and update " \
-                "Visual Studio Code C/C++ properties for Mbed projects",
+    description="A command-line to facilitate vscode intellisense  for mbed-os.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sterngerlach/mbed-config-gen",
+    url="https://github.com/sterngerlach/mbed-vscode-tools",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
