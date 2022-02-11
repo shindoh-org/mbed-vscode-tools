f3d3aabe1bec5d0b4642c8e91a5e1574a9d5aa95
otenim <snake_istobelieve@keio.jp>
2022-02-12 05:40:53 +0900

    update

--- mbed_vscode_tools/mbed_vscode_tools.py
+++ mbed_vscode_tools/mbed_vscode_tools.py
@@ -7,9 +7,9 @@ import subprocess
 
 TOOL_CONFFILE_NAME = '.mbed-vscode-tools.json'
 TOOL_CONFFILE_INDENT_LENGTH = 4
-BUILD_CMAKE_ROOTDIR_NAME = 'cmake_build'
-BUILD_CMAKE_CONFFILE_NAME = 'mbed_config.cmake'
-BUILD_NINJA_CONFFILE_NAME = 'build.ninja'
+CMAKE_ROOTDIR_NAME = 'cmake_build'
+CMAKE_CONFFILE_NAME = 'mbed_config.cmake'
+NINJA_BUILDFILE_NAME = 'build.ninja'
 
 
 @click.group()
@@ -18,15 +18,15 @@ def cmd():
 
 
 @cmd.command()
-@click.argument('toolchain', type=click.Choice(['GCC_ARM', 'ARM']))
+@click.argument('mbed-toolchain', type=click.Choice(['GCC_ARM', 'ARM']))
 @click.argument('mbed-target', type=str)
 @click.option(
-    '--profile',
+    '--mbed-profile',
     type=click.Choice(['debug', 'develop', 'release']),
     default='develop', show_default=True,
-    help='Choose a build profile.')
+    help='Choose an mbed build profile.')
 @click.option(
-    '--program-path',
+    '--mbed-program-path',
     type=click.Path(
         exists=True, file_okay=False, dir_okay=True,
         resolve_path=True, path_type=pathlib.Path),
@@ -34,50 +34,58 @@ def cmd():
     help='Path to an mbed program directory. '
     'If not specified, it\'s set to your working directory.')
 def configure(
-        toolchain: str, mbed_target: str,
-        profile: str, program_path: pathlib.Path) -> None:
+        mbed_toolchain: str, mbed_target: str,
+        mbed_profile: str, mbed_program_path: pathlib.Path) -> None:
     """Configure build settings.
 
-    [TOOLCHAIN] The toolchain you are using to build your mbed application.
+    [MBED_TOOLCHAIN] The toolchain you are using to build your mbed application.
     Choose \'GCC_ARM\' or \'ARM\'.
 
     [MBED_TARGET] A build target for an mbed-enabled device (e.g. DISCO_L072CZ_LRWAN1).
     """
 
-    cmake_build_dir = program_path / BUILD_CMAKE_ROOTDIR_NAME / mbed_target / profile / toolchain
-    cmake_conf = cmake_build_dir / BUILD_CMAKE_CONFFILE_NAME
+    click.echo('[Configure]')
+    cmake_build_dir = mbed_program_path / CMAKE_ROOTDIR_NAME / mbed_target / mbed_profile / mbed_toolchain
+    cmake_conf_file = cmake_build_dir / CMAKE_CONFFILE_NAME
+    ninja_build_file = cmake_build_dir / NINJA_BUILDFILE_NAME
 
     # Check if cmake build directory exists
     if not cmake_build_dir.exists():
         raise Exception(
             f'Could not find the cmake build directory {cmake_build_dir} . '
-            f'Run \'$ mbed-tools configure\' properly first.')
+            'Run \'$ mbed-tools configure\' properly first.')
+    click.echo('---- CMake build directory check done.')
 
     # Check if cmake configuration file exists
-    if not cmake_conf.exists():
+    if not cmake_conf_file.exists():
         raise Exception(
-            f'Could not find the cmake configuration file {cmake_conf} . '
-            f'Run \'$ mbed-tools configure\' properly first.')
-
-    # Save config json file
-    config = {
-        'toolchain': toolchain,
-        'mbed_target': mbed_target,
-        'profile': profile,
-        'program_path': str(program_path)}
-    with (program_path / TOOL_CONFFILE_NAME).open('w') as file:
-        json.dump(config, file, indent=TOOL_CONFFILE_INDENT_LENGTH)
+            f'Could not find the cmake configuration file {cmake_conf_file} . '
+            'Run \'$ mbed-tools configure\' properly first.')
+    click.echo('---- Cmake configuration file check done.')
 
     # Generate build.ninja
     ret = subprocess.run([
         'cmake',
-        '-S', str(program_path),
+        '-S', str(mbed_program_path),
         '-B', str(cmake_build_dir),
         '-GNinja'], capture_output=True)
     if ret.returncode != 0:
         raise Exception(
             'Failed to generate build.ninja for some reasons. '
             'Below is the error output generated from cmake;\n%s' % ret.stderr.decode('utf-8'))
+    click.echo(f'---- build.ninja generation done ({ninja_build_file}).')
+
+    # Save config json file
+    tool_conf_file = mbed_program_path / TOOL_CONFFILE_NAME
+    conf = {
+        'mbed_toolchain': mbed_toolchain,
+        'mbed_target': mbed_target,
+        'mbed_profile': mbed_profile,
+        'program_path': str(mbed_program_path)}
+    with tool_conf_file.open('w') as file:
+        json.dump(conf, file, indent=TOOL_CONFFILE_INDENT_LENGTH)
+    click.echo(f'---- Tool configuration file saved ({tool_conf_file})')
+    click.echo(f'---- Configuration finished!')
 
 
 @cmd.command()
