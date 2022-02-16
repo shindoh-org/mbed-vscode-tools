1e5dde0108904318f9d09d205a73d6aa3a0c1ca4
otenim <snake_istobelieve@keio.jp>
2022-02-16 21:07:19 +0900

    update

--- mbed_vscode_tools/consts.py
+++ mbed_vscode_tools/consts.py
@@ -9,14 +9,4 @@ NINJA_BUILDFILE_NAME = 'build.ninja'
 VSCODE_CONFFILE_NAME = 'c_cpp_properties.json'
 VSCODE_CONFFILE_INDENT_LENGTH = 4
 VSCODE_CONFENTRY_BASE = 'Mbed'
-
-# Identify os type
-_os_type = platform.system()
-if _os_type == 'Darwin':
-    VSCODE_CONFENTRY_AUTO = 'Mac'
-elif _os_type == 'Windows':
-    VSCODE_CONFENTRY_AUTO = 'Win32'
-elif _os_type == 'Linux':
-    VSCODE_CONFENTRY_AUTO = 'Linux'
-else:
-    raise Exception(f'Unexpected platform ({_os_type}) detected.')
+VSCODE_CONFENTRY_AUTO = 'MbedAuto'
