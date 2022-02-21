d3c713688a33c03e50462a7809b8d34057954659
otenim <snake_istobelieve@keio.jp>
2022-02-21 23:36:09 +0900

    improved log sentences

--- mbed_vscode_tools/mbed_vscode_tools.py
+++ mbed_vscode_tools/mbed_vscode_tools.py
@@ -32,7 +32,7 @@ def cmd():
         exists=True, file_okay=True, dir_okay=False,
         resolve_path=True, path_type=pathlib.Path),
     help='Path to an arm compiler to use. '
-    'If not set, the reuslt of \"$ which arm-none-eabi-gcc\" is used.')
+    'If not set, the output of \"$ which arm-none-eabi-gcc\" is used.')
 def generate(
         out_dir: pathlib.Path,
         c_standard: str,
@@ -44,8 +44,10 @@ def generate(
 
     [OUR_DIR] The output directory where a template of your c_cpp_properties.json
     will be generated. If the specified directory doesn't exist,
-    it'll be recursively created including sub-directories involved.
+    it'll be created including sub-directories involved.
     """
+    click.echo(click.style('[GENERATE]', bold=True))
+
     # Check out_dir exists
     if not out_dir.exists():
         # If it doesn't, create out_dir including subdirectories
@@ -53,6 +55,7 @@ def generate(
         click.echo(
             f'-- Could not find the output directory ({out_dir}).\n'
             '   The directory was newly created including sub-directories.')
+    click.echo(f'-- The output directory ({out_dir}) found.')
 
     # Find arm compiler path
     if compiler_path is None:
@@ -67,9 +70,10 @@ def generate(
                 'If not installed, try \"$ sudo apt install gcc-arm-none-eabi\".\n'
                 f'Here\'s the error output from which command >>\n{err}')
         compiler_path = ret.stdout.decode('utf-8').strip()
+        compiler_executable = compiler_path.split('/')[-1]
         click.echo(f'-- The default arm compiler (arm-none-eabi-gcc) found at <{compiler_path}>.')
     else:
-        click.echo(f'-- The specified arm compiler ({compiler_path}) found.')
+        click.echo(f'-- The specified arm compiler ({compiler_executable}) found at <{compiler_path}>.')
 
     # Create c_cpp_properties.json
     vscode_conf = {
@@ -89,10 +93,10 @@ def generate(
     out_path = out_dir / consts.VSCODE_CONFFILE_NAME
     with (out_path).open('w') as file:
         json.dump(vscode_conf, file, indent=consts.VSCODE_CONFFILE_INDENT_LENGTH)
-        click.echo(f'-- Saved c_cpp_properties.json as <{out_path}>.')
+        click.echo(f'-- Saved your c_cpp_properties.json at <{out_path}>.')
 
     # Success
-    click.echo(click.style('[GENERATE SUCCEEDED]', fg='green', bold=True))
+    click.echo('-- ' + click.style('SUCCEEDED', fg='green', bold=True))
 
 
 @cmd.command()
@@ -135,6 +139,7 @@ def configure(
     You can generate a template of your c_cpp_properties.json by \"$ mbed-vscode-tools generate\" command
     for quick start.
     """
+    click.echo(click.style('[CONFIGURE]', bold=True))
 
     cmake_build_dir = \
         mbed_program_dir / \
@@ -147,6 +152,7 @@ def configure(
     # Load c_cpp_properties.json
     with vscode_conf_file.open(mode='r') as file:
         vscode_conf = json.load(file)
+    click.echo(f'-- Your c_cpp_properties.json ({vscode_conf_file}) found and loaded.')
 
     # Check validity of c_cpp_properties.json
     n = len(list(filter(  # The number of base entries (must be only one)
@@ -160,18 +166,21 @@ def configure(
         raise Exception(
             f'More than two \"{consts.VSCODE_CONFENTRY_BASE}\" config entries found in {vscode_conf_file}. '
             f'Leave one \"{consts.VSCODE_CONFENTRY_BASE}\" config entry and remove the others.')
+    click.echo(f'-- No errros found in your c_cpp_properties.json')
 
     # Check if cmake build directory exists
     if not cmake_build_dir.exists():
         raise Exception(
             f'Could not find the cmake build directory ({cmake_build_dir}). '
             'Run \"$ mbed-tools configure\" first.')
+    click.echo(f'-- The cmake build directory ({cmake_build_dir}) found.')
 
     # Check if cmake configuration file exists
     if not cmake_conf_file.exists():
         raise Exception(
             f'Could not find the cmake config file ({cmake_conf_file}). '
             'Run \"$ mbed-tools configure\" first.')
+    click.echo(f'-- The cmake config file ({cmake_conf_file}) found.')
 
     # Generate build.ninja
     ret = subprocess.run([
@@ -184,6 +193,7 @@ def configure(
         raise Exception(
             'Failed to generate build.ninja for some reasons. '
             f'Here\'s the error output from cmake >>\n{err}')
+    click.echo(f'-- Succeeded to generate build.ninja.')
 
     # Save config json file
     tool_conf_file = mbed_program_dir / consts.TOOL_CONFFILE_NAME
@@ -198,8 +208,10 @@ def configure(
         'ninja_build_file': str(cmake_build_dir / consts.NINJA_BUILDFILE_NAME)}
     with tool_conf_file.open('w') as file:
         json.dump(conf, file, indent=consts.TOOL_CONFFILE_INDENT_LENGTH)
-    click.echo(click.style('[CONFIGURE SUCCEEDED]', fg='green', bold=True))
-    click.echo(f'Your tool config file was saved as <{tool_conf_file}>.')
+    click.echo(f'-- Saved your tool config file at <{tool_conf_file}>.')
+
+    # Success
+    click.echo('-- ' + click.style('SUCCEEDED', fg='green', bold=True))
 
 
 @cmd.command()
@@ -209,33 +221,34 @@ def configure(
         exists=True, file_okay=True, dir_okay=False,
         resolve_path=True, path_type=pathlib.Path),
     default=(pathlib.Path().cwd() / consts.TOOL_CONFFILE_NAME), show_default=True,
-    help=f'Path to the tool configuration file ({consts.TOOL_CONFFILE_NAME}) generated by configure command. '
+    help=f'Path to the tool config file ({consts.TOOL_CONFFILE_NAME}) generated by configure command. '
          f'If not specified, it\'s set to ./{consts.TOOL_CONFFILE_NAME}')
 def update(tool_conf_file: pathlib.Path) -> None:
     """Update your c_cpp_properties.json
 
-    Arguments: update command has no positional arguments.
+    Arguments: This sub-command has no positional arguments.
     """
+    click.echo(click.style('[UPDATE]', bold=True))
 
-    # Check if tool configuration file exists
+    # Load tool config file
     if not tool_conf_file.exists():
         raise Exception(
-            f'Could not find your tool configuration file at <{tool_conf_file}>.'
+            f'Could not find your tool config file at <{tool_conf_file}>.'
             'Set a correct path into \'--tool-conf-path\' option, or '
             'run \"$ mbed-vscode-tools configure\" if you haven\'t done yet.')
-
-    # Load tool configuration file
     with tool_conf_file.open('r') as file:
         tool_conf = json.load(file)
-    vscode_conf_file = pathlib.Path(tool_conf['vscode_conf_file'])
-    cmake_build_dir = pathlib.Path(tool_conf['cmake_build_dir'])
+    click.echo(f'-- Your tool config file ({tool_conf_file}) found and loaded.')
 
     # Check if build.ninja exists
+    cmake_build_dir = pathlib.Path(tool_conf['cmake_build_dir'])
+    vscode_conf_file = pathlib.Path(tool_conf['vscode_conf_file'])
     ninja_build_file = pathlib.Path(tool_conf['ninja_build_file'])
     if not ninja_build_file.exists():
         raise Exception(
             f'Could not find build.ninja at <{ninja_build_file}>. '
             'Run \"$ mbed-vscode-tools configure\" first.')
+    click.echo(f'-- Found build.ninja at <{ninja_build_file}>.')
 
     # Parse build.ninja
     defines, includes = [], []
@@ -268,15 +281,17 @@ def update(tool_conf_file: pathlib.Path) -> None:
     # Manually add one include
     # TODO: Should parse this automatically as well
     includes.append(str(cmake_build_dir / '_deps' / 'greentea-client-src' / 'include'))
+    click.echo(f'-- Detected {len(includes)} include paths & {len(defines)} defines in your build.ninja.')
 
     # Load c_cpp_properties.json
     if not vscode_conf_file.exists():
         raise Exception(
             f'Could not find your c_cpp_properties.json at <{vscode_conf_file}>, '
-            f'though the tool configuration file ({tool_conf_file}) points the path. '
-            'Run \"$ mbed-vscode-tools configure\" again to fix the problem.')
+            f'though the tool config file ({tool_conf_file}) points it. '
+            'Run \"$ mbed-vscode-tools configure\" again to fix this problem.')
     with vscode_conf_file.open(mode='r') as file:
         vscode_conf = json.load(file)
+    click.echo(f'-- Your c_cpp_properties ({vscode_conf_file}) found and loaded.')
 
     # Get base entry
     base_entry = next(filter(
@@ -305,10 +320,10 @@ def update(tool_conf_file: pathlib.Path) -> None:
     vscode_conf['configurations'] = new_entries
     with vscode_conf_file.open('w') as file:
         json.dump(vscode_conf, file, indent=consts.VSCODE_CONFFILE_INDENT_LENGTH)
+    click.echo(f'-- Updated your c_cpp_properties.json.')
 
-    # Prompt
-    click.echo(click.style('[UPDATE SUCCEEDED]', fg='green', bold=True))
-    click.echo(f'Your c_cpp_properties.json ({tool_conf_file}) has been updated.')
+    # Success
+    click.echo('-- ' + click.style('SUCCEEDED', fg='green', bold=True))
 
 
 def main():
