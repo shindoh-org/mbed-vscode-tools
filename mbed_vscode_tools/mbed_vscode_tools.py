db6f89548640ffd96b30bc20b37c9580ae5f814f
otenim <snake_istobelieve@keio.jp>
2022-03-01 22:40:05 +0900

    update

--- mbed_vscode_tools/mbed_vscode_tools.py
+++ mbed_vscode_tools/mbed_vscode_tools.py
@@ -66,79 +66,54 @@ def cmd():
 
 
 @cmd.command()
-@click.argument('mbed-toolchain', type=click.Choice(['GCC_ARM', 'ARM']))
-@click.argument('mbed-target', type=str)
+@click.argument(
+    'mbed-build-dir',
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True,
+        resolve_path=True, path_type=pathlib.Path))
 @click.argument(
     'vscode-conf-file',
     type=click.Path(
         exists=True, file_okay=True, dir_okay=False,
         resolve_path=True, path_type=pathlib.Path))
 @click.option(
-    '--mbed-profile',
-    type=click.Choice(['debug', 'develop', 'release']),
-    default='develop', show_default=True,
-    help='Choose an mbed build profile.')
-@click.option(
     '--mbed-program-dir',
     type=click.Path(
         exists=True, file_okay=False, dir_okay=True,
         resolve_path=True, path_type=pathlib.Path),
     default=pathlib.Path().cwd(), show_default=True,
-    help='Path to your mbed program directory. '
+    help='Path to the mbed program directory root. '
          'If not specified, it\'s set to your current working directory.')
 @click.option(
     '--vscode-conf-entry',
-    type=str, default='Mbed', show_default=True,
+    type=str, default=consts.VSCODE_DEFAULT_CONFENTRY_NAME, show_default=True,
     help='Specify the target config entry of your c_cpp_properties.json.')
 @click.option(
     '--verbose', is_flag=True,
     help='Show complete message logs.')
-def configure(
-        mbed_toolchain: str, mbed_target: str, vscode_conf_file: pathlib.Path,
-        mbed_profile: str, mbed_program_dir: pathlib.Path, vscode_conf_entry: str, verbose: bool) -> None:
-    """Configure and save the build settings.
+def update(
+        mbed_build_dir: pathlib.Path, vscode_conf_file: pathlib.Path,
+        mbed_program_dir: pathlib.Path, vscode_conf_entry: str, verbose: bool) -> None:
+    """Update your c_cpp_properties.json.
 
-    [MBED_TOOLCHAIN] The toolchain you are using to build your mbed application.
-    Choose \"GCC_ARM\" or \"ARM\".
-
-    [MBED_TARGET] A build target for an mbed-enabled device (e.g. DISCO_L072CZ_LRWAN1).
+    [MBED_BUILD_DIR] The build directory created by \"$ mbed-tools configure -t MBED_TOOLCHAIN -m MBED_TARGET -b MBED_PROFILE\".
+    Normally the directory is <path-to-your-mbed-program-directory>/cmake_build/{MBED_TARGET}/{MBED_PROFILE}/{MBED_TOOLCHAIN}.
 
     [VSCODE_CONF_FILE] Path to your c_cpp_properties.json.
     Make sure that your c_cpp_properties.json has an config entry whose name == --vscode-conf-entry in \"configurations\" field.
     The entry is managed and updated by this tool for correct vscode intellisense.
     """
-    cmake_build_dir = \
-        mbed_program_dir / \
-        consts.CMAKE_ROOTDIR_NAME / \
-        mbed_target / \
-        mbed_profile / \
-        mbed_toolchain
-    cmake_conf_file = cmake_build_dir / consts.CMAKE_CONFFILE_NAME
-
-    # Load c_cpp_properties.json
-    with vscode_conf_file.open(mode='r') as file:
-        vscode_conf = json.load(file)
-    if verbose:
-        click.echo(f'-- Your c_cpp_properties.json ({vscode_conf_file}) found and loaded.')
-
     # Check validity of c_cpp_properties.json
     vscode_conf = validate_vscode_conf_file(vscode_conf_file, vscode_conf_entry)
     if verbose:
         click.echo(f'-- No errros found in your c_cpp_properties.json')
 
-    # Check if cmake build directory exists
-    if not cmake_build_dir.exists():
-        raise Exception(
-            f'Could not find the cmake build directory ({cmake_build_dir}). '
-            'Run \"$ mbed-tools configure\" first.')
-    if verbose:
-        click.echo(f'-- The cmake build directory ({cmake_build_dir}) found.')
-
     # Check if cmake configuration file exists
+    cmake_conf_file = mbed_build_dir / consts.CMAKE_CONFFILE_NAME
     if not cmake_conf_file.exists():
         raise Exception(
             f'Could not find the cmake config file ({cmake_conf_file}). '
-            'Run \"$ mbed-tools configure\" first.')
+            'Run the \"configure\" command of mbed-tools first.')
     if verbose:
         click.echo(f'-- The cmake config file ({cmake_conf_file}) found.')
 
@@ -146,7 +121,7 @@ def configure(
     ret = subprocess.run([
         'cmake',
         '-S', str(mbed_program_dir),
-        '-B', str(cmake_build_dir),
+        '-B', str(mbed_build_dir),
         '-GNinja'], capture_output=True)
     if ret.returncode != 0:
         err = ret.stderr.decode('utf-8')
@@ -162,98 +137,11 @@ def configure(
         vscode_conf['configurations']))
 
     # Update "Mbed" entry
-    ninja_build_file = cmake_build_dir / consts.NINJA_BUILDFILE_NAME
-    includes, defines = parse_includes_and_defines(ninja_build_file)
+    includes, defines = parse_includes_and_defines(mbed_build_dir / consts.NINJA_BUILDFILE_NAME)
     conf_entry['includePath'] = includes
     conf_entry['defines'] = defines
-
-    # Save c_cpp_properties.json
-    with vscode_conf_file.open('w') as file:
-        json.dump(vscode_conf, file, indent=consts.VSCODE_CONFFILE_INDENT_LENGTH)
-    click.echo(f'-- Updated your c_cpp_properties.json.')
-
-    # Save config json file
-    tool_conf_file = mbed_program_dir / consts.TOOL_CONFFILE_NAME
-    tool_conf = {  # TODO: convert to relative paths
-        'mbed_toolchain': mbed_toolchain,
-        'mbed_target': mbed_target,
-        'mbed_profile': mbed_profile,
-        'mbed_program_dir': str(mbed_program_dir),
-        'cmake_build_dir': str(cmake_build_dir),
-        'cmake_conf_file': str(cmake_conf_file),
-        'vscode_conf_file': str(vscode_conf_file),
-        'vscode_conf_entry': vscode_conf_entry,
-        'ninja_build_file': str(ninja_build_file)}
-    with tool_conf_file.open('w') as file:
-        json.dump(tool_conf, file, indent=consts.TOOL_CONFFILE_INDENT_LENGTH)
-    click.echo(f'-- Saved your tool config file as <{tool_conf_file}>.')
-
-    # Success
-    click.echo(click.style('[CONFIGURE DONE]', fg='green', bold=True))
-
-
-@cmd.command()
-@click.option(
-    '--tool-conf-file',
-    type=click.Path(
-        exists=True, file_okay=True, dir_okay=False,
-        resolve_path=True, path_type=pathlib.Path),
-    default=(pathlib.Path().cwd() / consts.TOOL_CONFFILE_NAME), show_default=True,
-    help=f'Path to the tool config file ({consts.TOOL_CONFFILE_NAME}) generated by configure command. '
-         f'If not specified, it\'s set to ./{consts.TOOL_CONFFILE_NAME}')
-@click.option(
-    '--verbose', is_flag=True,
-    help='Show complete message logs.')
-def update(tool_conf_file: pathlib.Path, verbose: bool) -> None:
-    """Update your c_cpp_properties.json"""
-
-    # Load tool config file
-    if not tool_conf_file.exists():
-        raise Exception(
-            f'Could not find your tool config file at <{tool_conf_file}>.'
-            'Set a correct path into \'--tool-conf-path\' option, or '
-            'run \"$ mbed-vscode-tools configure\" if you haven\'t done yet.')
-    with tool_conf_file.open('r') as file:
-        tool_conf = json.load(file)
-    if verbose:
-        click.echo(f'-- Your tool config file ({tool_conf_file}) found and loaded.')
-
-    # Check if build.ninja exists
-    vscode_conf_file = pathlib.Path(tool_conf['vscode_conf_file'])
-    ninja_build_file = pathlib.Path(tool_conf['ninja_build_file'])
-    if not ninja_build_file.exists():
-        raise Exception(
-            f'Could not find build.ninja at <{ninja_build_file}>. '
-            'Run \"$ mbed-vscode-tools configure\" first.')
-    if verbose:
-        click.echo(f'-- Found build.ninja at <{ninja_build_file}>.')
-
-    # Parse build.ninja
-    includes, defines = parse_includes_and_defines(ninja_build_file)
-    if verbose:
-        click.echo('-- ' + click.style(f'{len(includes)}', fg='white', bold=True) + ' include paths parsed.')
-        click.echo('-- ' + click.style(f'{len(defines)}', fg='white', bold=True) + ' defines parsed.')
-
-    # Load c_cpp_properties.json
-    if not vscode_conf_file.exists():
-        raise Exception(
-            f'Could not find your c_cpp_properties.json at <{vscode_conf_file}>, '
-            f'though the tool config file ({tool_conf_file}) points it. '
-            'Run \"$ mbed-vscode-tools configure\" again to fix this problem.')
-    with vscode_conf_file.open(mode='r') as file:
-        vscode_conf = json.load(file)
-    if verbose:
-        click.echo(f'-- Your c_cpp_properties ({vscode_conf_file}) found and loaded.')
-
-    # Get target config entry
-    vscode_conf_entry = tool_conf['vscode_conf_entry']
-    conf_entry = next(filter(
-        lambda entry: entry['name'] == vscode_conf_entry,
-        vscode_conf['configurations']))
-
-    # Update target config entry
-    conf_entry['includePath'] = includes
-    conf_entry['defines'] = defines
+    click.echo(f'-- {len(includes)} include paths parsed.')
+    click.echo(f'-- {len(defines)} defines parsed.')
 
     # Save c_cpp_properties.json
     with vscode_conf_file.open('w') as file:
@@ -261,7 +149,7 @@ def update(tool_conf_file: pathlib.Path, verbose: bool) -> None:
     click.echo(f'-- Updated your c_cpp_properties.json.')
 
     # Success
-    click.echo(click.style('[UPDATE DONE]', fg='green', bold=True))
+    click.echo(click.style('UPDATE DONE', fg='green', bold=True))
 
 
 def main():
