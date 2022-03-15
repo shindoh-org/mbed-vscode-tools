fccbb524c2d6954cc0e45a42a74f0363420af502
otenim <snake_istobelieve@keio.jp>
2022-03-15 23:12:30 +0900

    change version restriction from click>=8.0.0 to 8.0.0>click>=7.0.0

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ with open(os.path.join(curdir, 'README.md'), 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='mbed-vscode-tools',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
-    install_requires=['click>=8.0.0'],
+    install_requires=['click>=7.0.0, <8.0.0'],
     entry_points={
         'console_scripts': [
             'mbed-vscode-tools=mbed_vscode_tools.mbed_vscode_tools:main']
